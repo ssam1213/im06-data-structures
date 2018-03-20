8fc35de5ad25f35d52ee9a907d1abd2a76b65547
deanheo <hideanheo@gmail.com>
2018-03-20 15:50:58 +0900

    init

---
+++ sprint-one/src/functional/stack.js
@@ -0,0 +1,21 @@
+var Stack = function() {
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var count = 0;
+
+  // Implement the methods below
+  someInstance.push = function(value) {
+    count++;
+  };
+
+  someInstance.pop = function() {
+  };
+
+  someInstance.size = function() {
+    return count;
+  };
+
+  return someInstance;
+};
